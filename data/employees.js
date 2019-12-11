f5fbf6baf5d5ad6adffc697819ab713342290a67
Cynthia Wong <43305867+cynwong@users.noreply.github.com>
2019-12-11 18:42:57 +1100

    add data

---
+++ data/employees.js
@@ -0,0 +1,72 @@
+module.exports = [
+    {
+        name: "Jared",
+        role: "Manager",
+        id: 1,
+        email:"jared@fakemail.com",
+        officeNumber: 1
+    },
+    {
+        name: "Alec",
+        role: "Engineer",
+        id: 2,
+        email:"alec@fakemail.com",
+        github: "ibealec"
+    },
+    {
+        name: "Tammer",
+        role: "Engineer",
+        id: 3,
+        email:"tammer@fakemail.com",
+        github: "tammerg"
+    },
+    {
+        name: "Christian",
+        role: "Engineer",
+        id: 4,
+        email:"christian@fakemail.com",
+        github: "ceckenrode"
+    },
+    {
+        name: "John",
+        role: "Intern",
+        id: 5,
+        email:"john@fakemail.com",
+        school: "2University"
+    },
+    {
+        name: "Juliet",
+        role: "Manager",
+        id: 6,
+        email:"juliet@fakemail.com",
+        officeNumber: 100
+    },
+    {
+        name: "Jane",
+        role: "Intern",
+        id: 7,
+        email:"jane@fakemail.com",
+        school: "2University"
+    },
+    {
+        name: "Simon",
+        role: "Intern",
+        id: 8,
+        email:"simno@fakemail.com",
+        school: "2University"
+    },
+    {
+        name: "Chrissy",
+        role: "Engineer",
+        id: 9,
+        email:"chrissy@fakemail.com",
+        github: "chrissy9990"
+    },
+    {
+        name: "Derek",
+        role: "Manager",
+        id: 10,
+        email:"derek@fakemail.com",
+        officeNumber: 25
+    },
+];
