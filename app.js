ed951f791280dd7ec4cd0bf91078d6ae5f5a67d7
Cynthia Wong <43305867+cynwong@users.noreply.github.com>
2019-12-12 22:02:47 +1100

    move process and path out from app.js

--- app.js
+++ app.js
@@ -1,91 +1,13 @@
 const inquirer = require("inquirer");
-const fs = require("fs");
-const ejs = require("ejs");
-const util = require("util");
-const path = require("path");
-const open = require("open");
-const writeFileAsync = util.promisify(fs.writeFile);
 
+const createRoster = require("./lib/process");
 
 const EMPLOYEES = require("./lib/access_data");
 const Manager = require("./lib/manager");
 
 //separate managers from the rest
 const managers = EMPLOYEES.filter((employee)=> employee instanceof Manager);
-const members = EMPLOYEES.filter((employee)=> !(employee instanceof Manager));
-
-const OUTPUT_FOLDER = path.join(__dirname,"output");
-
-const getFilename = (username)=>{
-    username = username.replace(" ", "_").toLowerCase();
-    return  `${username}_${new Date().getTime()}.html`;
-};
-
-const createRoster = async (user)=>{
-    try{
-
-        //create the team array with the manager
-        const team = [user];
-
-        // get destination file path
-        const filename = getFilename(user.name);
-        const filePath = path.join(OUTPUT_FOLDER, filename);
-
-        //ask user to select the team members.
-        const getMemberNamesFromUser = async ()=>{
-            //add separator in console
-            console.log("-".repeat(80));
-            const {teamMembers} = await inquirer.prompt([
-                {
-                    name:"teamMembers",
-                    message: "Select the team members",
-                    choices: members.filter(({name,available})=> {
-                        if(available){
-                            return name;
-                        }
-                    }),
-                    type: "checkbox"
-                }
-            ]);
-            let names = teamMembers; 
-            if(names.length === 0){
-                console.log("\n*** No staff member is selected. At least one member must be selected.\n");
-                name = await getMemberNamesFromUser();
-            }
-            return names; 
-        };
-        
-        const names = await getMemberNamesFromUser();
-        
-        members.forEach((member)=>{
-            //check if a engineer or an intern is selected
-            if(names.indexOf(member.name) !== -1){
-                //if selected, change the member status to be assgined
-                member.assigned();
-                //add to the team. 
-                team.push(member);
-            }
-        });
-        
-        //render team info to html by using template. 
-        const html = await ejs.renderFile("templates/main.ejs", 
-            {
-                team,
-            }
-        );
-        //write file to the output folder
-        console.log("Saving file...")
-        await writeFileAsync(filePath, html);
-
-        //open output file in the browser for the user. 
-        console.log(`${filename} file is saved in the folder ${OUTPUT_FOLDER}.`)
-        await open(filePath);
-    }catch(err){
-        console.log("Error in creating roster", err.message);
-        process.exit();
-    }
-};
-
+let members = EMPLOYEES.filter((employee)=> !(employee instanceof Manager));
 
 const start = async () => {
     try{
@@ -105,7 +27,7 @@ const start = async () => {
         console.log(`Hello, ${user.getName()}.`);
         do{
             //create roster
-            await createRoster(user);
+            members = await createRoster(user,members);
             //check if all staff members have been assigned to a team.
             if(members.every(({available})=>!available)){
                 // if yes, then cannot create more roster so exit the program. 
